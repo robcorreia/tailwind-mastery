3fc3fcba026179d9f63c85b98ae635c32680e953
Robson <robcorreia@gmail.com>
2024-01-15 21:32:10 -0300

    feat: create project

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="p-8 bg-slate-900 text-slate-100 h-screen">
+      <h1 className="font-bold text-5xl">Hello Tailwind</h1>
+      <h2>Hello Tailwind</h2>
+      <h3>Hello Tailwind</h3>
+    </div>
+  );
+}
